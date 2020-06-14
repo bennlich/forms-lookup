af9b4d9d2b9146f002e7b605d41d21f0b8c4ab65
Benny Lichtner <bennlich@gmail.com>
2020-06-14 12:07:44 -0700

    link to form info pages

--- courts.ca.gov/src/form-lookup-drupal-api.js
+++ courts.ca.gov/src/form-lookup-drupal-api.js
@@ -137,14 +137,18 @@ export async function initFormsLookup(containerEl) {
   }
 
   function formResult(form) {
+    let formInfoUrl = `https://epic-forms-jcc-srl.pantheonsite.io/jcc-form/${form.id
+      .toLowerCase()
+      .replace(/\(|\)/g, "")}`;
+
     return html`
       <div class="jcc-forms-filter__form-result">
         <div class="jcc-forms-filter__form-result-content">
-          <a class="jcc-forms-filter__form-number-and-title" href="${form.url}" target="_blank">
+          <a class="jcc-forms-filter__form-number-and-title" href="${formInfoUrl}" target="_blank">
             <div class="form-number">${form.id}</div>
             <div class="form-title">${form.title}</div>
           </a>
-          <a class="usa-button usa-button--outline jcc-forms-filter__form-guide-button" href="${form.url}" target="_blank">See form info</a>
+          <a class="usa-button usa-button--outline jcc-forms-filter__form-guide-button" href="${formInfoUrl}" target="_blank">See form info</a>
           <a class="usa-button usa-button--outline jcc-forms-filter__download-form-button" href="${form.url}" target="_blank">Download form</a>
         </div>
       </div>
