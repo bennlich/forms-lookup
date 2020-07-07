2dd2a25b66e05d8c45650d9089a45cf90e51e4e6
Benny Lichtner <bennlich@gmail.com>
2020-07-07 15:13:54 -0700

    make category alert case-insensitive

--- red-dot-integration/src/components/CategoryAlert.js
+++ red-dot-integration/src/components/CategoryAlert.js
@@ -2,12 +2,12 @@ import html from 'nanohtml/lib/browser';
 import { categories } from '../categories.js';
 
 export const CategoryAlert = ({ query }) => {
-  let category = categories.find(category => category.query === query);
+  let category = categories.find(category => category.query.toLowerCase() === query.toLowerCase());
   if (category) {
     return html`
       <div class="usa-alert usa-alert--info" >
         <div class="usa-alert__body">
-          <p class="usa-alert__heading jcc-forms-filter__alert-heading">Looking for info about ${query}?</p>
+          <p class="usa-alert__heading jcc-forms-filter__alert-heading">Looking for info about ${query.toLowerCase()}?</p>
           <p class="usa-alert__text">
             Read the <a href="${category.url}">${category.title} self-help guide</a> and the <a href="${category.formsUrl}">${category.title} forms page</a>.
           </p>
