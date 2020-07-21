f32ded04763de47d4e4a63629de53cee9fe65663
Benny Lichtner <bennlich@gmail.com>
2020-07-21 15:47:39 -0700

    hide forms packet links if they don't exist

--- red-dot-integration/src/components/CategoryAlert.js
+++ red-dot-integration/src/components/CategoryAlert.js
@@ -3,14 +3,14 @@ import { categories } from '../categories.js';
 
 export const CategoryAlert = ({ query }) => {
   let category = categories.find(category => category.query.toLowerCase() === query.toLowerCase());
-  if (category) {
+  if (category && category.url) {
     return html`
       <div class="usa-alert usa-alert--info" >
         <div class="usa-alert__body">
           <p class="usa-alert__heading jcc-forms-filter__alert-heading">Looking for info about ${category.title.toLowerCase()}?</p>
           <ul class="usa-alert__text">
             <li>Read the <a href="${category.url}">${category.title} self-help guide</a></li>
-            <li>See <a href="${category.formsUrl}">${category.title} forms packets</a></li>
+            ${category.formsUrl ? html`<li>See <a href="${category.formsUrl}">${category.title} forms packets</a></li>` : ''}
           </ul>
         </div>
       </div>`;  
