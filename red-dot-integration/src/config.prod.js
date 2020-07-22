0b667c078d442872a55ab95517802b3fe857fc1c
Mel Wong <mel.wong@jud.ca.gov>
2020-07-22 09:42:14 -0700

    Production URLs adjustments

--- red-dot-integration/src/config.prod.js
+++ red-dot-integration/src/config.prod.js
@@ -3,10 +3,10 @@ export const lookupPageUrl = '/forms.htm';
 
 // The URL of the new all forms page
 // TODO: decide what this url will be
-export const allFormsPageUrl = '/allforms.html';
+export const allFormsPageUrl = '/allforms.htm';
 
 // The URL of the old dropdown lookup page
-export const legacyDropdownLookupUrl = '';
+export const legacyDropdownLookupUrl = '/forms-by-category.htm';
 
 // Set to false if, after the pilot period, you decide to hide the link to the old UI
 export const showLegacyDropdownLookupLink = true;
